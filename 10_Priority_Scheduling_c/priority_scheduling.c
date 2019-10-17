21e1dc93877cffce755c964f06917c29c70239a3
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-10-17 23:04:47 +0530

    Update Support Add Win Support

--- 10_Priority_Scheduling_c/priority_scheduling.c
+++ 10_Priority_Scheduling_c/priority_scheduling.c
@@ -1,85 +1,59 @@
-#include "stdio.h"
-#include "stdlib.h"
-struct process
+#include<stdio.h>
+#include<conio.h>
+void main()
 {
-	int process_id;
-	int arrival_time;
-	int burst_time;
-	int waiting_time;
-	int turn_around_time;
-	int priority;
-};
-int main()
+ int nop,t,wt[10],twt,tat[10],ttat,i,j,p[10],b[10],tmp;
+ float awt, atat;
+ clrscr();
+ awt=0.0;
+ atat=0.0;0
+ printf("Enter the number of process:");
+ scanf("%d",&nop);
+for(i=0;i<nop;i++)
 {
-	int n,i,j;
-	int bt=0,k=1,tat=0,sum=0,min,mini;
-	printf("Enter number of processes: ");
-	scanf("%d",&n);
-	struct process proc[n],temp;
-	for(i=0;i<n;i++)
-	{
-		printf("\n");
-		printf("Enter arrival time for process%d: ",i+1);
-		scanf("%d",&proc[i].arrival_time);
-		printf("Enter burst time for process%d: ",i+1);
-		scanf("%d",&proc[i].burst_time);
-		printf("Enter priority for process%d: ",i+1);
-		scanf("%d",&proc[i].priority);
-		proc[i].process_id = i+1;
-	}
-
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			if(proc[i].arrival_time < proc[j].arrival_time)
-			{
-				temp = proc[j];
-				proc[j] = proc[i];
-				proc[i] = temp;
-			}
-		}
-	}
-
-	for(i=0;i<n;i++)
-	{
-		bt+=proc[i].burst_time;
-		min = proc[k].burst_time;
-		mini = proc[k].priority;
-		for(j=k;j<n;j++)
-		{
-			if(bt>=proc[j].arrival_time && proc[j].priority<mini)
-			{
-				temp=proc[k];
-				proc[k]=proc[j];
-				proc[j]=temp;
-			}
-		}
-		k++;
-	}
-	proc[0].waiting_time=0;
-	int wait_time_total=0;
-	int turn_around_time_total=0;
-	for(i=1;i<n;i++)
-	{
-		sum+=proc[i-1].burst_time;
-		proc[i].waiting_time = sum-proc[i].arrival_time;
-		wait_time_total += proc[i].waiting_time;
-	}
-	for(i=0;i<n;i++)
-	{
-		tat+=proc[i].burst_time;
-		proc[i].turn_around_time = tat - proc[i].arrival_time;
-		turn_around_time_total+=proc[i].turn_around_time;
-	}
-	printf("Process\tBurst Time\tArrival Time\tWaiting Time\tTurn-Around Time\n");
-
-	for(i=0;i<n;i++)
-	{
-
-		printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",proc[i].process_id,proc[i].burst_time, proc[i].arrival_time, proc[i].waiting_time,proc[i].turn_around_time);
-	}
-	printf("Average waiting time: %f\n", (float)wait_time_total/n);
-	printf("Average turn around time: %f\n",(float)turn_around_time_total/n);
-
+ printf("Enter the burst time of  Process %d:",i);
+ scanf("%d",&b[i]);
 }
+for(i=0;i<nop;i++)
+ printf("Enter the priority number of each Process %d:",i);
+ scanf("%d",&p[i]);
+}
+ for(i=0;i<nop;i++)
+ {
+  for(j=i+1;j<nop;j++)
+  {
+   if(p[i]>p[j])
+   {
+    t=p[i];
+    p[i]=p[j];
+    p[j]=t;
+    tmp=b[i];
+    b[i]=b[j];
+    b[j]=tmp;
+   }
+  }
+ }
+ wt[0]=0;
+ tat[0]=b[0];
+ twt=wt[0];
+ ttat=tat[0];
+ for(i=1;i<nop;i++)
+ {
+  wt[i]=wt[i-1]+b[i-1];
+  tat[i]=wt[i]+b[i];
+  twt+=wt[i];
+  ttat+=tat[i];
+ }
+ awt=(float)twt/nop;
+ atat=(float)ttat/nop;
+ printf("Process No:\tPriority:\tBurst Time:\tWaiting Time\tTurnaround Time:\n");
+ for(i=0;i<nop;i++)
+  printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i,p[i],b[i],wt[i],tat[i]);
+
+ printf("Total TurnAround Time:%d\n",ttat);
+ printf("Total Waiting Time:%d\n",twt);
+ printf("Average Waiting Time:%f\n",awt);
+ printf("Average Turnaround Time:%f\n",atat);
+ getch();
+}
+
